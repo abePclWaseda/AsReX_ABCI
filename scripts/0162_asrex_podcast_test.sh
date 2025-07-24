00dee6cecaf4faa973c3a4ac8ece9e5ae909b79f
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-07-24 21:18:24 +0900

    スクリプトの修正: 0162_asrex_podcast_test.shでPBSオプションを追加し、asrex_podcast_test.pyで引数処理を実装。音声ファイルの処理を改善。

--- scripts/0162_asrex_podcast_test.sh
+++ scripts/0162_asrex_podcast_test.sh
@@ -4,6 +4,7 @@
 #PBS -v RTYPE=rt_HF,USE_SSH=1
 #PBS -l select=1:ngpus=1
 #PBS -l walltime=3:00:00
+#PBS -j oe
 
 set -eu
 
@@ -20,4 +21,4 @@ echo "==== which python ===="
 which python               
 python --version     
 
-exec python -m asrex_podcast_test
+exec python -m tools.asrex_podcast_test --dirs 00000-of-00001
